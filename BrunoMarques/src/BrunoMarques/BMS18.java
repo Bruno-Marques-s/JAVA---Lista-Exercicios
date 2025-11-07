d7a943f0b093a30305b9339dff6b539647651980
Bruno Marques <brunomsantos71@gmail.com>
2025-11-07 11:48:29 -0300

    Update BMS18.java

--- BrunoMarques/src/BrunoMarques/BMS18.java
+++ BrunoMarques/src/BrunoMarques/BMS18.java
@@ -6,7 +6,7 @@ public class BMS18 {
 		// TODO Auto-generated method stub
 		System.out.println("************************************");
 		System.out.println("*ALuno: Bruno Marques - RA: 0023647*");
-		System.out.println("*Classe BMS17 - Múltiplos de 3 entre 1 e 30*");
+		System.out.println("*Classe BMS18 - Múltiplos de 3 entre 1 e 30*");
 		System.out.println("************************************");
 		
 		 System.out.println("Múltiplos de 3 entre 1 e 30:");
